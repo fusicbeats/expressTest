6669025581a9a39ee29b727e9f0f10114240ff02
sayamghosh <140226407+fusicbeats@users.noreply.github.com>
2024-07-21 00:26:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use("/",(req,res)=>{
     res.send("Hello My name is donald trump");
 })
 
+app.use("/about",(req,res)=>{
+    res.send("I am going to be the precident");
+})
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 })
